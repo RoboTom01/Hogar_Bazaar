7b4ffea6f1d1990b5f3e3480683aa127b850573c
Thomas <tmrtw8@gmail.com>
2023-03-22 20:27:33 -0600

    03222023-1

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.get( async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const userData = await User.findAll();
-
+    console.log(userData)
     return res.json(userData)
 
     // if (!userData) {
