45fb2bdb2d7ca18e636c7e26afeb8132a1208e39
dukanegolder <dukanegolder@gmail.com>
2023-03-27 19:10:47 -0600

    creating new links in navbar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
   try {
@@ -16,7 +16,8 @@ router.get("/", withAuth, async (req, res) => {
 router.get("/postItem", withAuth, async (req, res) => {
   try {
     res.render("create-items", {
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
+      style: "",
     });
   } catch (err) {
     res.status(500).json(err);
